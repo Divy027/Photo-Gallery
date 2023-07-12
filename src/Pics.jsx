a1d033f9dd6912d3e9b4e30953da78b8060d920c
Divy027 <105413340+Divy027@users.noreply.github.com>
2023-07-12 18:45:57 +0530

    Update Pics.jsx

--- src/Pics.jsx
+++ src/Pics.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Pics = ({ src,FullSrc, alt, photographer,profileURL, description })=> {
+const Pics = ({ src,FullSrc, alt, photographer,profileURL, description })=> {  //using props
     return (
+        // main div
         <div className="p-4"> 
-            <div className="relative  group">
+            {/* pic div */}
+           <div className="relative  group">   
                 <img  src={src} alt={alt} className="w-full h-32 object-cover rounded"/>
 
                 <a href={FullSrc} target="_blank" className=" absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-white text-center opacity-0 group-hover:opacity-100 transition-opacity">
@@ -16,7 +18,7 @@ const Pics = ({ src,FullSrc, alt, photographer,profileURL, description })=> {
 
              </div>
             
-
+            {/* description and profile div  */}
             <div className="flex justify-between items-center"> 
                 <div className="mr-2 "> 
                  <img className=" rounded-3xl h-10 w-10 " src={profileURL}/> 
